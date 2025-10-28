7b3f2051fd3cbd75aba61c608da6598791e8351b
endijuan33 <endcorepath@gmail.com>
2025-10-28 18:16:41 +0700

    add missing dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,15 @@ module.exports = {
     "prettier",
     "plugin:@typescript-eslint/recommended",
   ],
-  plugins: ["@typescript-eslint", "react-refresh"],
+  plugins: ["@typescript-eslint", "react-refresh", "import"],
   rules: {
     "react-refresh/only-export-components": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": [
       "error",
-      { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
-    ]
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+    ],
+
+    "import/no-unresolved": "error"
   },
 };
