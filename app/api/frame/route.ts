17df3953aca7d0b682aab8b6762398251fd48201
endijuan33 <endcorepath@gmail.com>
2025-10-31 16:54:06 +0700

    feat: add frame for farcaster minia-app

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,18 +1,15 @@
-import { NextResponse } from "next/server";
-
 export async function GET() {
-  return new NextResponse(null, {
-    status: 200,
-    headers: {
-      "fc:frame": "vNext",
-      "fc:frame:image": "https://ethtools.vercel.app/placeholder-logo.png",
-      "fc:frame:image:aspect_ratio": "1.91:1",
-      "fc:frame:button:1": "Open App",
-      "fc:frame:button:1:action": "link",
-      "fc:frame:button:1:target": "https://ethtools.vercel.app",
-      "fc:frame:button:2": "GitHub",
-      "fc:frame:button:2:action": "link",
-      "fc:frame:button:2:target": "https://github.com/endijuan33",
-    },
-  });
+  const headers = new Headers();
+
+  headers.set("fc:frame", "vNext");
+  headers.set("fc:frame:image", "https://ethtools.vercel.app/placeholder-logo.png");
+  headers.set("fc:frame:image:aspect_ratio", "1.91:1");
+  headers.set("fc:frame:button:1", "Open App");
+  headers.set("fc:frame:button:1:action", "link");
+  headers.set("fc:frame:button:1:target", "https://ethtools.vercel.app");
+  headers.set("fc:frame:button:2", "GitHub");
+  headers.set("fc:frame:button:2:action", "link");
+  headers.set("fc:frame:button:2:target", "https://github.com/endijuan33");
+
+  return new Response(null, { status: 200, headers });
 }
