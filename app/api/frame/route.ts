d3d401aeea0f074586e950cf36a8e9013f276cc5
endijuan33 <endcorepath@gmail.com>
2025-10-31 17:01:14 +0700

    feat: add frame for farcaster minia-app

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,15 +1,24 @@
 export async function GET() {
-  const headers = new Headers();
-
-  headers.set("fc:frame", "vNext");
-  headers.set("fc:frame:image", "https://ethtools.vercel.app/placeholder-logo.png");
-  headers.set("fc:frame:image:aspect_ratio", "1.91:1");
-  headers.set("fc:frame:button:1", "Open App");
-  headers.set("fc:frame:button:1:action", "link");
-  headers.set("fc:frame:button:1:target", "https://ethtools.vercel.app");
-  headers.set("fc:frame:button:2", "GitHub");
-  headers.set("fc:frame:button:2:action", "link");
-  headers.set("fc:frame:button:2:target", "https://github.com/endijuan33");
-
-  return new Response(null, { status: 200, headers });
+  return new Response(
+    `<!DOCTYPE html>
+<html>
+  <head>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="https://ethtools.vercel.app/placeholder-logo.png" />
+    <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
+    <meta property="fc:frame:button:1" content="Open App" />
+    <meta property="fc:frame:button:1:action" content="link" />
+    <meta property="fc:frame:button:1:target" content="https://ethtools.vercel.app" />
+    <meta property="fc:frame:button:2" content="GitHub" />
+    <meta property="fc:frame:button:2:action" content="link" />
+    <meta property="fc:frame:button:2:target" content="https://github.com/endijuan33" />
+  </head>
+</html>`,
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    }
+  );
 }
